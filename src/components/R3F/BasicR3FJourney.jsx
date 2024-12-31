d91c9845dbd94c098e713423225aec7c7ce790f6
AathiEswar <aathi28082003@gmail.com>
2024-12-31 08:49:58 +0530

    auto commit

--- src/components/R3F/BasicR3FJourney.jsx
+++ src/components/R3F/BasicR3FJourney.jsx
@@ -1,6 +1,7 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import * as THREE from 'three'
 
 export default function BasicR3FJourney() {
   return (
@@ -14,11 +15,24 @@ export default function BasicR3FJourney() {
 
 function Model() {
   return (
-    <mesh>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshNormalMaterial />
-      <OrbitControls enableZoom={false} />
-    </mesh>
+    <group>
+      <mesh position={[2,0,0]}>
+        {/* args is for sending values during construction */}
+        <boxGeometry args={[2, 2, 2]}/>
+        <meshNormalMaterial />
+        <OrbitControls enableZoom={false} />
+      </mesh>
+      <mesh position={[-2,0,0]}>
+        <sphereGeometry />
+        <meshNormalMaterial />
+      </mesh>
+      <mesh scale={10} rotation-x={-Math.PI * 0.5} position={[0,-1,0]}>
+        <planeGeometry/>
+        <meshNormalMaterial
+          // side={THREE.DoubleSide}
+        />
+      </mesh>
+    </group>
   )
 }
 
