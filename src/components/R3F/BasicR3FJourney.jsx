d5ced03f331d56f0a30e75dff3102f086c70a409
AathiEswar <aathi28082003@gmail.com>
2024-12-31 09:07:10 +0530

    manual orbitcontrols

--- src/components/R3F/BasicR3FJourney.jsx
+++ src/components/R3F/BasicR3FJourney.jsx
@@ -1,7 +1,12 @@
-import { OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
+import { Canvas, extend, useFrame, useThree } from '@react-three/fiber'
+import React, { useRef } from 'react'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+
+// for converting a native imperative js to declaritive jsx
+extend({ OrbitControls })
+
 
 export default function BasicR3FJourney() {
   return (
@@ -13,23 +18,34 @@ export default function BasicR3FJourney() {
   )
 }
 
+// Some hooks of r3f only works inside Canvas
 function Model() {
+  const cubeRef = useRef();
+
+  const { camera, gl } = useThree();
+
+  useFrame((state, delta) => {
+    // delta is the difference in renders in milliseconds
+    cubeRef.current.rotation.y += delta
+  })
+
   return (
     <group>
-      <mesh position={[2,0,0]}>
+      <mesh position={[2, 0, 0]} ref={cubeRef}>
         {/* args is for sending values during construction */}
-        <boxGeometry args={[2, 2, 2]}/>
+        <boxGeometry args={[2, 2, 2]} />
         <meshNormalMaterial />
-        <OrbitControls enableZoom={false} />
+        <orbitControls args={[camera, gl.domElement]} damping={20} />
+        {/* <OrbitControls enableZoom={false} /> */}
       </mesh>
-      <mesh position={[-2,0,0]}>
+      <mesh position={[-2, 0, 0]}>
         <sphereGeometry />
         <meshNormalMaterial />
       </mesh>
-      <mesh scale={10} rotation-x={-Math.PI * 0.5} position={[0,-1,0]}>
-        <planeGeometry/>
+      <mesh scale={10} rotation-x={-Math.PI * 0.5} position={[0, -1, 0]}>
+        <planeGeometry />
         <meshNormalMaterial
-          // side={THREE.DoubleSide}
+        // side={THREE.DoubleSide}
         />
       </mesh>
     </group>
