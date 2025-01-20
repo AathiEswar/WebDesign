3507af3e42ed58888cf43da0b0bcd2cf4cf0a242
aathieswar-fs <aathi28082003@gmail.com>
2025-01-20 17:48:09 +0530

    loaders

--- src/components/R3F/newModels/LoadModles.jsx
+++ src/components/R3F/newModels/LoadModles.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import { Clone, OrbitControls, useGLTF } from '@react-three/drei'
+import { Clone, OrbitControls, useAnimations, useGLTF } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { Canvas } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
@@ -20,6 +20,10 @@ import PlaceHolder from '../PlaceHolder'
     5. useGLTF - GLTF loader from react three drei , also supports draco files 
     6. preload - property of useGLTF function , use to preload the model 
     7. Clone - effective cloning of the models by using same geometry and material ( used instead of primitive )
+
+
+    Animations : 
+    1. useAnimations - to manipulate animations of the 3d models
 */
 
 function LoadModles() {
@@ -52,6 +56,7 @@ function Model() {
 
       <Suspense fallback={<PlaceHolder scale={[2, 3, 2]} position={[0, 2, 0]} />}>
         <HelmetModel />
+        <FoxModel />
       </Suspense>
 
 
@@ -60,6 +65,16 @@ function Model() {
         <meshPhysicalMaterial color="greenyellow" />
       </mesh>
 
+    </>
+  )
+}
+
+function FoxModel() {
+  const model = useGLTF("/models/Fox/glTF/Fox.gltf")
+  const foxAnim = useAnimations(model.animations , model.scene)
+  return (
+    <>
+      <primitive object={model.scene} scale={0.03} position={[0, 0, 3]} />
     </>
   )
 }
@@ -77,9 +92,9 @@ function HelmetModel() {
   const model = useGLTF('/models/FlightHelmet/glTF/FlightHelmet.gltf')
   return (
     <>
-      <Clone object={model.scene} scale={5} castShadow position={[4, 0, 0]} />
-      <Clone object={model.scene} scale={5} castShadow position={[0, 0, 0]} />
-      <Clone object={model.scene} scale={5} castShadow position={[-4, 0, 0]} />
+      <Clone object={model.scene} scale={5} castShadow position={[4, 0, -1]} />
+      <Clone object={model.scene} scale={5} castShadow position={[0, 0, -1]} />
+      <Clone object={model.scene} scale={5} castShadow position={[-4, 0, -1]} />
     </>
   )
 }
